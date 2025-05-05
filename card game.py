37963f1eb3e9c3fc2da94d271d44c1e00f11efb4
Ollie <olliebobhiggins@gmail.com>
2025-05-05 19:24:29 +0100

    fixed

--- card game.py
+++ card game.py
@@ -2,8 +2,8 @@ import pgzrun as pz
 from random import randint
 
 #screen size
-WIDTH = 450
-HEIGHT = 450
+WIDTH = 800
+HEIGHT = 600
 
 
     
@@ -21,70 +21,60 @@ vlade = Actor("vlade")
 cheat = Actor("cheat")
 
 #positions
-scrooble.pos(250, 230)
-trant.pos(250, 230)
-foobie.pos(250, 230)
-Sranty.pos(250, 230)
-bentle.pos(250, 230)
-lobur.pos(250, 230)
-flamth.pos(250, 230)
-vlade.pos(250, 230)
-cheat.pos(250, 230)
-boxcoin.pos(35, 225)
+scrooble.pos = (250, 230)
+trant.pos = (250, 230)
+foobie.pos = (250, 230)
+Sranty.pos = (250, 230)
+bentle.pos = (250, 230)
+lobur.pos = (250, 230)
+flamth.pos = (250, 230)
+vlade.pos = (250, 230)
+cheat.pos = (250, 230)
+boxcoin.pos = (35, 225)
 
 #draw
 def draw():
     screen.fill("grey")
     boxcoin.draw
 
-#randomise
-def on_mouse_down():
-    if scrooble.collidepoint:
-        randint(1, 10)
-        if randint == 1: #scrooble
-            print("you got a Scrooble!") 
-            def draw():
-                screen.clear
-                scrooble.draw
-        elif randint == 2: #trant
-            print("you got Trant!")
-            def draw():
-                screen.clear
-                trant.draw
-        elif randint == 3: #foobie
-            print("you got Foobie!")
-            def draw():
-                screen.clear
-                foobie.draw
-        elif randint == 4: #sranty
-            print("you got Sranty!")
-            def draw():
-                screen.clear
-                Sranty.draw
-        elif randint == 5: #bentle
-            print("you got Bentle!")
-            def draw():
-                screen.clear
-                bentle.draw
-        elif randint == 6: #lobur
-            print("you got Lobur!")
-            def draw():
-                screen.clear
-                lobur.draw
-        elif randint == 7: #flamth
-            print("you got Flamth!")
-            def draw():
-                screen.clear
-                flamth.draw
-        elif randint == 8: #vlade
-            print("you got vlade!")
-            def draw():
-                screen.clear
-                vlade.draw
-        elif randint == 9: #cheat
-            print("you got cheat")
-            def draw():
-                screen.clear
-                cheat.draw
+current_actor = None  # Add this at the top of your script
+
+def draw():
+    screen.fill("grey")
+    boxcoin.draw()
+    if current_actor:
+        current_actor.draw()
+
+def on_mouse_down(pos):
+    global current_actor
+    if scrooble.collidepoint(pos):
+        result = randint(1, 10)
+        if result == 1:
+            print("You got a Scrooble!")
+            current_actor = scrooble
+        elif result == 2:
+            print("You got Trant!")
+            current_actor = trant
+        elif result == 3:
+            print("You got Foobie!")
+            current_actor = foobie
+        elif result == 4:
+            print("You got Sranty!")
+            current_actor = Sranty
+        elif result == 5:
+            print("You got Bentle!")
+            current_actor = bentle
+        elif result == 6:
+            print("You got Lobur!")
+            current_actor = lobur
+        elif result == 7:
+            print("You got Flamth!")
+            current_actor = flamth
+        elif result == 8:
+            print("You got Vlade!")
+            current_actor = vlade
+        elif result == 9:
+            print("You got Cheat!")
+            current_actor = cheat
 #run
-pz.go
+pz.go()
